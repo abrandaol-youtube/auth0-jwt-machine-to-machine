db642743aeb9f73d475092d449e7d3ff0345892d
Alexandre Brandão <alexandre.brandao.lustosa@gmail.com>
2020-06-19 20:24:43 -0300

    First commit

---
+++ Auth0-JWT-M2M/Auth0-JWT-M2M/Controllers/HealthCheckController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth0_JWT_M2M.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetInfo()
+        {
+            return Ok($"{DateTime.UtcNow:o}");
+        }
+    }
+}
